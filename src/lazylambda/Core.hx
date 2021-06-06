572648f73cfc10ea1b408e890d108be00fb08cf9
BJogart <jasper.opti@gmail.com>
2021-06-07 00:32:00 +0200

    add shorthands for simplicity

--- src/lazylambda/Core.hx
+++ src/lazylambda/Core.hx
@@ -9,6 +9,7 @@ typedef MapIterator<A, B> = lazylambda.core.MapIterator<A, B>;
 typedef RangeIterError = lazylambda.core.RangeIterator.RangeIterError;
 typedef RangeIterator = lazylambda.core.RangeIterator;
 typedef Sequentials = lazylambda.core.Sequentials;
+typedef Shorthands = lazylambda.core.Shorthands;
 typedef Slice<C, T> = lazylambda.core.Slice<C, T>;
 typedef Terminals = lazylambda.core.Terminals;
 typedef ZipIterator<A, B> = lazylambda.core.ZipIterator<A, B>;
---
+++ src/lazylambda/core/Shorthands.hx
@@ -0,0 +1,42 @@
+package lazylambda.core;
+
+using tink.CoreApi;
+using lazylambda.Core;
+
+class Shorthands {
+    public static inline function iterConcat<T>(iter: Iterable<T>, ...iters: Iterator<T>): Iterator<T> {
+        return iter.iterator().concat(...iters);
+    }
+
+    public static inline function iterFilter<T>(iter: Iterable<T>, pred: T -> Bool): Iterator<T> {
+        return iter.iterator().filter(pred);
+    }
+
+    public static inline function iterFlatMap<A, B>(iter: Iterable<A>, fun: A -> Iterator<B>): Iterator<B> {
+        return iter.iterator().flatMap(fun);
+    }
+
+    public static inline function iterIndexed<T>(iter: Iterable<T>): KeyValueIterator<Int, T> {
+        return iter.iterator().indexed();
+    }
+
+    public static inline function iterKeys<K, V>(iter: KeyValueIterable<K, V>): Iterator<K> {
+        return iter.keyValueIterator().keys();
+    }
+
+    public static inline function iterValues<K, V>(iter: KeyValueIterable<K, V>): Iterator<V> {
+        return iter.keyValueIterator().values();
+    }
+
+    public static inline function iterMap<A, B>(iter: Iterable<A>, fun: A -> B): Iterator<B> {
+        return iter.iterator().map(fun);
+    }
+
+    public static inline function iterZip<A, B>(iter1: Iterable<A>, iter2: Iterator<B>): KeyValueIterator<A, B> {
+        return iter1.iterator().zip(iter2);
+    }
+
+    public static inline function iterZipWithResult<T, R>(iter: Iterable<T>, fun: T -> R): KeyValueIterator<T, R> {
+        return iter.iterator().zipWithResult(fun);
+    }
+}
